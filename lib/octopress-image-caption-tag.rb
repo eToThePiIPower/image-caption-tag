486bc4efebd352685eef1fb51c63371001c482b9
Charles Beynon <eToThePiIPower@gmail.com>
2015-11-22 01:35:42 -0500

    Refactoring and better testing of block version

--- lib/octopress-image-caption-tag.rb
+++ lib/octopress-image-caption-tag.rb
@@ -83,48 +83,66 @@ module Octopress
         @height = ''
 
         def initialize(tag_name, markup, tokens)
-          if markup =~ /(\S.*\s+)?(https?:\/\/|\/)(\S+)(\s+\d+%?\s+\d+%?)?(\s+.+)?/i
-            @class = Regexp.last_match(1) || ''
-            @img = Regexp.last_match(2) + Regexp.last_match(3)
-            @title = Regexp.last_match(5).strip if Regexp.last_match(5)
-            if Regexp.last_match(4) =~ /\s*(\d+%?)\s+(\d+%?)/
-              @width = Regexp.last_match(1)
-              @height = Regexp.last_match(2)
-            elsif @class.rstrip == 'right' or @class.rstrip == 'left'
-              @width = '33%'
-            else
-              @width = '100%'
-            end
-            if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ @title
-              @title  = title
-              @alt    = alt
-            else
-              @alt    = @title.gsub!(/"/, '&#34;') if @title
-            end
+          if %r{(?<classname>\S.*\s+)?(?<protocol>https?://|/)(?<url>\S+)(?<sizes>\s+\d+%?\s+\d+%?)?(?<title>\s+.+)?} =~ markup
+            @class = classname || 'center'
+            @img = "#{protocol}#{url}"
+            @title = title.strip if title
+            parse_sizes(sizes)
+            parse_title(@title)
           end
           super
         end
 
         def render(context)
-          @caption = super
           site = context.registers[:site]
-          converter = site.getConverterImpl(Jekyll::Converters::Markdown)
+          converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+          @caption = converter.convert(super).lstrip.rstrip
           if @img && @width[-1] == '%' # Relative width, so width goes on outer span
-            "<figure class='#{('caption-wrapper ' + @class).rstrip}' style='width:#{@width};'>" \
-              "<a class='image-popup' href='#{@img}'>" \
-              "<img class='caption' src='#{@img}' width='100%' height='100%' title='#{@title}' alt='#{@alt}'>" \
-              '</a>' \
-              "<figurecaption class='caption-text'>#{@caption}</figurecaption>" \
-              '</figure>'
+            <<-EOS.gsub(/^ {12}/, '') # gsubm is to pretty up source by indenting
+            <figure class='caption-wrapper #{@class.rstrip}' style='width:#{@width};'>
+              <a class='image-popup' href='#{@img}'>
+                <img class='caption' src='#{@img}' width='100%' height='100%' title='#{@title}' alt='#{@alt}'>
+              </a>
+              <figurecaption class='caption-text'>
+            #{@caption}
+              </figurecaption>
+            </figure>
+            EOS
           elsif @img # Absolute width, so width goes on the img tag and text span gets sytle-width:@width-15;
-            "<figure class='#{('caption-wrapper ' + @class).rstrip}'>" \
-              "<a class='image-popup' href='#{@img}'>" \
-              "<img class='caption' src='#{@img}' width='#{@width}px' height='#{@height}px' title='#{@title}' alt='#{@alt}'>" \
-              '</a>' \
-              "<figurecaption class='caption-text' style='width:#{@width.to_i - 10}px;'>#{converter.convert(@caption)}</figurecaption>" \
-              '</figure>'
+            <<-EOS.gsub(/^ {12}/, '') # gsubm is to pretty up source by indenting
+            <figure class='caption-wrapper #{@class.rstrip}'>
+              <a class='image-popup' href='#{@img}'>
+                <img class='caption' src='#{@img}' width='#{@width}px' height='#{@height}px' title='#{@title}' alt='#{@alt}'>
+              </a>
+              <figurecaption class='caption-text' style='width:#{@width.to_i - 10}px;'>
+            #{@caption}
+              </figurecaption>
+            </figure>
+            EOS
           else
             'Error processing input, expected syntax: {% imgcaption [class name(s)] /url/to/image [width height] [title [alt]] %} Caption Text {% endimgcaption %}'
+          end
+        end
+
+        def parse_sizes(raw_size)
+          if /\s*(?<w>\d+%?)\s+(?<h>\d+%?)/ =~ raw_size
+            @width = w
+            @height = h
+          elsif @class.rstrip == 'right' || @class.rstrip == 'left'
+            @width = '33%'
+          else
+            @width = '100%'
+          end
+        end
+
+        def parse_title(raw_title)
+          if /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ raw_title
+            @title  = title
+            @alt    = alt
+          else
+            /(?:"|')(?<titlealt>[^"']+)?(?:"|')/ =~ raw_title
+            @title = titlealt
+            @alt = titlealt
           end
         end
       end
@@ -136,12 +154,12 @@ Liquid::Template.register_tag('imgcap', Octopress::Tags::ImageCaptionTag::Tag)
 Liquid::Template.register_tag('imgcaption', Octopress::Tags::ImageCaptionTag::Block)
 
 if defined? Octopress::Docs
-  Octopress::Docs.add({
+  Octopress::Docs.add(
     name:        'Octopress Image Caption Tag',
     gem:         'octopress-image-caption-tag',
     description: 'A tag to create images with pretty captions for Jekyll and Octopress blogs',
     path:        File.expand_path(File.join(File.dirname(__FILE__), '../')),
     source_url:  'https://github.com/eToThePiIPower/octopress-image-caption-tag',
     version:     Octopress::Tags::ImageCaptionTag::VERSION
-  })
+  )
 end
